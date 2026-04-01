faf45c5ebc52f821bdc561b0d7d34c473250430d
cecinuga <cecinuga.tdb@gmail.com>
2026-04-01 23:43:44 +0200

    added permutation handling

--- solver.c
+++ solver.c
@@ -25,7 +25,7 @@ void partial_pivoting(double **A, size_t *permutation, const size_t rank){
     for (size_t i = 0; i < rank; i++){
         max_i = i;
         max = dabs(A[i][i]);
-        for (int j = i; j < rank; j++){
+        for (size_t j = i; j < rank; j++){
             if ( dabs(A[j][i]) > max ){
                 max_i = j;
                 max = dabs(A[j][i]);
@@ -78,7 +78,7 @@ void elimination(double **coefs, double **U, double **L, const size_t rank){
  * consts (RHS vector b) is allocated but unused -- no solve step exists yet.
  */
 int main(void) {
-    const size_t rank = 5;
+    const size_t rank = 3;
 
     size_t *permutation = calloc(rank, sizeof(size_t));
     double **coefs = calloc(rank, sizeof(double*)); 
@@ -86,11 +86,11 @@ int main(void) {
     double **lower = matrix_alloc(rank, rank);
     double *consts = calloc(rank, sizeof(double)); 
 
-    double col0[] = {1, 2, 5, 6, -4};
-    double col1[] = {3, 8, 10, -3, -8};
-    double col2[] = {-4.5, -4, 3, -4, 9.3}; 
-    double col3[] = {-5, 4, 1.36, 7.89, -9.69};
-    double col4[] = {1, -7, 2, 12, -1.5};
+    double col0[] = {1, 2, 5};
+    double col1[] = {3, 8, 10};
+    double col2[] = {-4.5, -4, 3}; 
+    double col3[] = {-5, 4, 1.36};
+    double col4[] = {1, -7, 2};
 
     coefs[0] = (double*)&col0;
     coefs[1] = (double*)&col1;
@@ -98,13 +98,20 @@ int main(void) {
     coefs[3] = (double*)&col3;
     coefs[4] = (double*)&col4;
 
-    for (int i = 0; i < rank; i++ ) 
+    for (size_t i = 0; i < rank; i++ ) 
         permutation[i] = i;
 
     matrix_copy(upper, coefs, rank);
     eye(lower, rank);
 
     printf("original matrix: \n");
+    print_matrix(coefs, rank);
+
+    printf("permutation flatten matrix: \n");
+    print_stvector(permutation, rank);
+
+    printf("applied permutation matrix: \n");
+    perm_matrix(coefs, permutation, rank);
     print_matrix(coefs, rank);
 
     printf("------------------------------\n");
@@ -114,7 +121,7 @@ int main(void) {
 
     printf("------------------------------\n");
     printf("\npermutation flatted matrix: \n");
-    print_ivector(permutation, rank);
+    print_stvector(permutation, rank);
 
     elimination(coefs, upper, lower, rank);
 
@@ -126,6 +133,10 @@ int main(void) {
     printf("\nlower triangle: \n");
     print_matrix(lower, rank);
 
-    matrix_free(lower, rank); matrix_free(upper, rank);
-    free(permutation); free(coefs); free(consts);
+    free(permutation); free(consts);
+    matrix_free(coefs, rank); matrix_free(lower, rank); matrix_free(upper, rank);
+
+    return 0;
 }
+
+
